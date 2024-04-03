fc049d5e864ff3e6a779c0a51bdc6a4a3a5dbce8
Jared Holgate <jaredholgate@microsoft.com>
2024-04-03 17:55:16 +0100

    enhancement-ephemeral-agents (#2)

--- container-images/azure-devops-agent/start.sh
+++ container-images/azure-devops-agent/start.sh
@@ -95,4 +95,4 @@ print_header "4. Running Azure Pipelines agent..."
 
 # `exec` the node runtime so it's aware of TERM and INT signals
 # AgentService.js understands how to handle agent self-update and restart
-exec ./externals/node/bin/node ./bin/AgentService.js interactive
+exec ./externals/node/bin/node ./bin/AgentService.js interactive --once
