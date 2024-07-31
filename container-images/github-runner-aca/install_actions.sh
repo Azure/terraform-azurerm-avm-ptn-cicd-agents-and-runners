fe5ebbbd84a633880583d57bff0b796849a1663e
Jared Holgate <jaredholgate@microsoft.com>
2024-07-31 13:14:25 +0100

    Fix version

--- container-images/github-runner-aca/install_actions.sh
+++ container-images/github-runner-aca/install_actions.sh
@@ -13,7 +13,10 @@ if [[ $GH_RUNNER_VERSION == "" ]]; then
   echo "GitHub Runner latest version: $GH_RUNNER_VERSION"
 fi
 
-curl -L "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-${TARGET_ARCH}-${GH_RUNNER_VERSION}.tar.gz" > actions.tar.gz
+ReplaceWith=""
+GH_RUNNER_VERSION=Result=${GH_RUNNER_VERSION/v/${ReplaceWith}}
+
+curl -L "https://github.com/actions/runner/releases/download/${GH_RUNNER_VERSION}/actions-runner-linux-${TARGET_ARCH}-${GH_RUNNER_VERSION}.tar.gz" > actions.tar.gz
 tar -zxf actions.tar.gz
 rm -f actions.tar.gz
 ./bin/installdependencies.sh
--- variables.container.registry.tf
+++ variables.container.registry.tf
@@ -72,7 +72,7 @@ variable "default_image_repository_url" {
 variable "default_image_repository_commit" {
   type        = string
   description = "The default image repository commit to use if no custom image is provided."
-  default     = "0e83d1d"
+  default     = "df7ea55"
 }
 
 variable "default_image_repository_folder_paths" {
