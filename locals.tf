68bdb076d424f7f07fa4b6b573ea9b02398b26a0
Jared Holgate <jaredholgate@microsoft.com>
2024-08-01 19:25:47 +0100

    Remove tests and bug fix

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   container_instance_subnet_id                     = var.use_private_networking && local.deploy_container_instance ? (var.create_virtual_network ? module.virtual_network[0].subnets["container_instance"].resource_id : var.container_instance_subnet_id) : ""
   container_registry_dns_zone_id                   = var.use_private_networking ? (var.create_container_registry_private_dns_zone ? azurerm_private_dns_zone.container_registry[0].id : var.container_registry_dns_zone_id) : ""
   container_registry_private_endpoint_subnet_id    = var.use_private_networking ? (var.create_virtual_network ? module.virtual_network[0].subnets["container_registry_private_endpoint"].resource_id : var.container_registry_private_endpoint_subnet_id) : ""
-  log_analytics_workspace_id                       = var.create_log_analytics_workspace ? module.log_analytics_workspace[0].resource_id : var.log_analytics_workspace_id
+  log_analytics_workspace_id                       = local.deploy_container_app && var.create_log_analytics_workspace ? module.log_analytics_workspace[0].resource_id : var.log_analytics_workspace_id
   resource_group_id                                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.resource_group_name}"
   resource_group_name                              = var.resource_group_creation_enabled ? azurerm_resource_group.this[0].name : var.resource_group_name
   resource_group_name_container_app_infrastructure = var.container_app_infrastructure_resource_group_name == null ? "rg-${var.postfix}-container-apps-infrastructure" : var.container_app_infrastructure_resource_group_name
