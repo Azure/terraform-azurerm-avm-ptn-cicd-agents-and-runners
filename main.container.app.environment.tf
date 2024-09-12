0d70cde76409196ae94b72edf7cda1a4b6f3473e
Jared Holgate <jaredholgate@microsoft.com>
2024-09-12 11:22:41 +0100

    chore: use shared container image repo (#58)
    * Use shared container image repo
    
    * linting
    
    * Fix idempotency issue

--- main.container.app.environment.tf
+++ main.container.app.environment.tf
@@ -4,7 +4,7 @@ resource "azurerm_container_app_environment" "this" {
   location                           = var.location
   name                               = local.container_app_environment_name
   resource_group_name                = local.resource_group_name
-  infrastructure_resource_group_name = local.resource_group_name_container_app_infrastructure
+  infrastructure_resource_group_name = var.use_private_networking ? local.resource_group_name_container_app_infrastructure : null
   infrastructure_subnet_id           = var.use_private_networking ? local.container_app_subnet_id : null
   internal_load_balancer_enabled     = var.use_private_networking ? true : null
   log_analytics_workspace_id         = local.log_analytics_workspace_id
@@ -14,5 +14,7 @@ resource "azurerm_container_app_environment" "this" {
   workload_profile {
     name                  = "Consumption"
     workload_profile_type = "Consumption"
+    maximum_count         = 0
+    minimum_count         = 0
   }
 }
